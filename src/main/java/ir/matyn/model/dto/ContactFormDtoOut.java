964783ba10473834bff2e5435169b121ab58eea9
GharehzadehAli <gharehzadehali@gmail.com>
2020-03-03 20:53:15 +0330

    I removed unnecessary annotations from ContactFormDtoOut and ContactFormEntity classes.

--- src/main/java/ir/matyn/model/dto/ContactFormDtoOut.java
+++ src/main/java/ir/matyn/model/dto/ContactFormDtoOut.java
@@ -2,25 +2,16 @@ package ir.matyn.model.dto;
 
 import lombok.Data;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
-
 @Data
 public class ContactFormDtoOut {
-    @NotBlank(message = "Please Enter An Id")
     private long id;
-    @NotBlank(message = "Please Enter A Name")
-    @Size(min = 3, max = 20)
+
     private String name;
-    @NotBlank(message = "Please Enter A Email")
-    @Email
+
     private String email;
-    @NotBlank(message = "Please Enter A Subject")
-    @Size(min = 3, max = 20)
+
     private String subject;
-    @NotBlank(message = "Please Enter A Message")
-    @Size(min = 3, max = 20)
+
     private String message;
 
 
--- src/main/java/ir/matyn/model/entity/ContactFormEntity.java
+++ src/main/java/ir/matyn/model/entity/ContactFormEntity.java
@@ -3,9 +3,6 @@ package ir.matyn.model.entity;
 import lombok.Data;
 
 import javax.persistence.*;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
 
 @Data
 @Entity
@@ -15,24 +12,18 @@ public class ContactFormEntity {
     @GeneratedValue
     @Column(name = "id")
     private long id;
-    @NotBlank(message = "Please Enter A Name")
-    @Size(min = 3, max = 20)
+
     @Column(name = "name", nullable = false)
     private String name;
-    @NotBlank(message = "Please Enter An Email")
-    @Email
+
     @Column(name = "email", nullable = false)
     private String email;
-    @NotBlank(message = "Please Enter A Subject")
-    @Size(min = 3, max = 20)
+
     @Column(name = "subject", nullable = false)
     private String subject;
-    @NotBlank(message = "Please Enter A Message")
-    @Size(min = 3, max = 50)
+    
     @Column(name = "message", nullable = false)
     private String message;
-
-
 
 
 }
